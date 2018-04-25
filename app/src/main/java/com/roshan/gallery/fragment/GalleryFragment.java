89c8a06f62cc90fbe1e28bbe0df802f14a4dae2c
Roshan Udayasiri Maddumage <uomroshan@gmail.com>
2018-04-25 20:03:15 +0530

    Gallery Fragment Updated
    Some Changes and bugs fixed

--- app/src/main/java/com/roshan/gallery/fragment/GalleryFragment.java
+++ app/src/main/java/com/roshan/gallery/fragment/GalleryFragment.java
@@ -1,23 +1,50 @@
 package com.roshan.gallery.fragment;
 
 
+import android.app.ProgressDialog;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.roshan.gallery.R;
+import com.roshan.gallery.adapter.GalleryAdapter;
+import com.roshan.gallery.app.AppController;
+import com.roshan.gallery.model.Image;
+import com.roshan.gallery.utills.GridSpacingItemDecoration;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class GalleryFragment extends Fragment {
 
-    private GridView gridView;
     View view;
-    public GalleryFragment(){}
+    private RecyclerView recyclerView;
+
+    private static final String url = "https://api.unsplash.com/photos/?client_id=cc506a9dd764cf4cc5bbe085fd95b7961afded842cd02c6ab3ea75bd5fea5514&per_page=20";
+    private ArrayList<Image> images;
+    private ProgressDialog pDialog;
+    private GalleryAdapter mAdapter;
+    private String TAG = GalleryFragment.class.getSimpleName();
+
+    public GalleryFragment() {
+
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,9 +55,69 @@ public class GalleryFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_gallery, container, false);
-        RecyclerView gridView = view.findViewById(R.id.recycler_view);
-        //gridView.setAdapter(new GalleryAdapter(view.getContext()));
+        pDialog = new ProgressDialog(getContext());
+        images = new ArrayList<>();
+        mAdapter = new GalleryAdapter(getContext(), images);
+
+        recyclerView = view.findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getContext(), 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, dpToPx(10), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        fetchImages();
         return view;
     }
 
+    private void fetchImages() {
+
+        pDialog.setMessage("Loading Images...");
+        pDialog.show();
+
+        JsonArrayRequest req = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        pDialog.hide();
+
+                        images.clear();
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject object = response.getJSONObject(i);
+                                Image image = new Image();
+                                //image.setName(object.getString("name"));
+                                image.setLike(object.getInt("likes"));
+
+                                JSONObject url = object.getJSONObject("urls");
+                                image.setSmall(url.getString("small"));
+                                image.setRegular(url.getString("regular"));
+                                image.setFull(url.getString("full"));
+                                //image.setTimestamp(object.getString("timestamp"));
+
+                                images.add(image);
+
+                            } catch (JSONException e) {
+                                Log.e(TAG, "Json parsing error: " + e.getMessage());
+                            }
+                        }
+
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Error: " + error.getMessage());
+                pDialog.hide();
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(req);
+    }
+
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
+    }
 }
